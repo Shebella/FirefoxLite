e81897c21a7c0239a503008e400bfe6ad81ba30f
Nevin Chen <cnevinchen@gmail.com>
2019-01-16 17:16:51 +0800

    Add telemetry when remote config completes fetching

--- app/src/main/java/org/mozilla/focus/telemetry/TelemetryWrapper.kt
+++ app/src/main/java/org/mozilla/focus/telemetry/TelemetryWrapper.kt
@@ -91,6 +91,7 @@ object TelemetryWrapper {
         const val OPEN = "open"
         const val SHOW = "show"
         const val LAUNCH = "launch"
+        const val INIT = "init"
     }
 
     internal object Object {
@@ -121,6 +122,7 @@ object TelemetryWrapper {
         const val BANNER = "banner"
         const val DOORHANGER = "doorhanger"
         const val VPN_DOORHANGER = "vpn_doorhanger"
+        const val FIREBASE = "firebase"
     }
 
     object Value {
@@ -306,6 +308,20 @@ object TelemetryWrapper {
                     .getDefaultSearchEngine(context)
                     .identifier
         }
+    }
+
+    @TelemetryDoc(
+            name = "Firebase RemoteConfig Fetched",
+            category = Category.ACTION,
+            method = Method.INIT,
+            `object` = Object.FIREBASE,
+            value = "null",
+            extras = [TelemetryExtra(name = Extra.TO, value = "true,false")]
+    )
+    @JvmStatic
+    fun firebaseRemoteConfigFetched() {
+        EventBuilder(Category.ACTION, Method.INIT, Object.FIREBASE)
+                .queue()
     }
 
     @TelemetryDoc(
